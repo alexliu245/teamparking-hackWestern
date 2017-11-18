c10268fa21dc4a71229e174e01e5b0c2646c7634
rafael.aguiar <rafael@alumni.ubc.ca>
2017-11-18 02:28:54 -0500

    feat(application): add a simple flask app

---
+++ application.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: say_hello()))
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
